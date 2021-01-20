6a93d58f59fbfe3d93be94a2d61a4e2094b372d9
manucho <manuel.blanco77@gmail.com>
2021-01-20 10:59:41 -0300

    Trabajando con strings, template strings

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
 "use strict";
 //ALGO DE BOOLEANS
 var day = 'friday';
-var superman = true;
-var batman;
-var aquaman = true;
+// let superman: boolean = true;
+// let batman: boolean;
+// let aquaman: boolean = true;
 //ALGO DE NUMBERS
 var avengers = 5;
 var villanos = 2;
 var otros = 2;
-if (avengers > villanos) {
-    console.log('estamos salvados');
-}
-else {
-    console.log('estamos muertos xD');
-}
-otros = 123.2;
+// if (avengers > villanos) {
+//   console.log('estamos salvados');
+// } else {
+//   console.log('estamos muertos xD');
+// }
+// otros = 123.2
 //ALGO DE STRINGS
+var batman = 'Batman';
+var linternaVerde = 'Linterna Verde';
+var panteraNegra = 'Pantera Negra';
+console.log(batman);
+console.log(linternaVerde);
+console.log(panteraNegra);
+var concatenar = "Los heroes son : " + batman + linternaVerde + panteraNegra;
+var concat = "Los heroes son: " + batman + ", " + linternaVerde + ", " + panteraNegra;
+console.log(concatenar);
+console.log(concat);
--- app.ts
+++ app.ts
@@ -2,11 +2,11 @@
 //ALGO DE BOOLEANS
 let day: string = 'friday';
 
-let superman: boolean = true;
+// let superman: boolean = true;
 
-let batman: boolean;
+// let batman: boolean;
 
-let aquaman: boolean = true;
+// let aquaman: boolean = true;
 
 //ALGO DE NUMBERS
 
@@ -15,13 +15,30 @@ let villanos: number = 2;
 
 let otros = 2;
 
-if (avengers > villanos) {
-  console.log('estamos salvados');
-} else {
-  console.log('estamos muertos xD');
-}
+// if (avengers > villanos) {
+//   console.log('estamos salvados');
+// } else {
+//   console.log('estamos muertos xD');
+// }
 
-otros = 123.2
+// otros = 123.2
 
 //ALGO DE STRINGS
 
+let batman: string = 'Batman';
+
+let linternaVerde: string = 'Linterna Verde';
+
+let panteraNegra: string = 'Pantera Negra';
+
+console.log(batman);
+console.log(linternaVerde);
+console.log(panteraNegra);
+
+let concatenar: string = "Los heroes son : " + batman + linternaVerde + panteraNegra;
+
+let concat: string = `Los heroes son: ${batman}, ${linternaVerde}, ${panteraNegra}`;
+
+
+console.log(concatenar);
+console.log(concat);
