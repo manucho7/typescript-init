dc439e4afc65f4b7939d8b7d2b3275b971c61851
manucho <manuel.blanco77@gmail.com>
2021-01-21 14:03:38 -0300

    Trabajando con classes y constructor() para inicializar variables

---
+++ classes.ts
@@ -0,0 +1,23 @@
+
+
+class Avenger {
+
+  nombre: string = "Sin nombre";
+  equipo: string = undefined;
+  nombreReal: string = undefined;
+
+  puedePelear: boolean = false;
+  peleasGanadas: number = 0;
+
+  constructor( nombre: string, equipo: string, nombreReal: string ) {
+    this.nombre = nombre;
+    this.equipo = equipo;
+    this.nombreReal = nombreReal;
+
+  }
+
+}
+
+let antman: Avenger = new Avenger( "Antman", "cap", "Scott Lang" );
+
+console.log(antman);
